d198aaf5c028d6b1ef88a86113db60d9648b15c0
Irina Safenkova <saf-iri@yandex.ru>
2022-07-02 16:30:29 +0300

    Реализована проверка уникальности job перед добавлением

--- crowleer/api/jobs.py
+++ crowleer/api/jobs.py
@@ -1,19 +1,27 @@
 import orjson
-import requests
+import httpx
 
-from crowleer.config import config
 from crowleer.api.schemas import Job
 
 
 class JobApi:
+    def __init__(self, url: str) -> None:
+        self.url = url
 
     def add(self, job: Job) -> Job:
         json_job = orjson.dumps(job.dict())
         headers = {
             'Content-Type': 'application/json',
         }
-        respose = requests.post(f'{config.app_host}:{config.app_port}/api/v1/company/jobs', content=json_job, headers=headers)
+        respose = httpx.post(f'{self.url}/api/v1/company/jobs', content=json_job, headers=headers)
         respose.raise_for_status()
 
         payload = respose.json()
         return Job(**payload)
+
+    def check_by_url(self, job: Job, url: Job.url) -> Job:
+        response = httpx.get(f'{self.url}/api/v1/company/jobs')
+        response.raise_for_status()
+        for job_in_base in response.json():
+            if url != job_in_base['url']:
+                return job
--- crowleer/api/schemas.py
+++ crowleer/api/schemas.py
@@ -1,15 +1,18 @@
 from datetime import date
 
+from typing import Optional
+
 from pydantic import BaseModel, Field
 
 
 class Job(BaseModel):
     uid: int = Field(ge=1)
     name: str = Field(min_length=2)
-    salary: int
-    description: str = Field(max_length=100)
+    salary: Optional[int]
+    region: Optional[str] = Field(min_length=2)
+    description: Optional[str] = Field(max_length=100)
     date_published: date
-    date_expiring: date
+    date_expiring: Optional[date]
     url: str
 
 
