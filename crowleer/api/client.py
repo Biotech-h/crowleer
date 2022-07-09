4fd916039d9d6de72a890534773869e54f442add
Vladimir Puzakov <vppuzakov@rambler.ru>
2022-07-09 13:17:06 +0300

    chore: remove companies from client

--- crowleer/api/client.py
+++ crowleer/api/client.py
@@ -1,9 +1,10 @@
-from crowleer.api.companies import CompanyApi
 from crowleer.api.jobs import JobApi
+from crowleer.config import config
+
 
 class ApiClient:
     def __init__(self) -> None:
-        self.jobs = JobApi()
-        self.companies = CompanyApi()
+        self.jobs = JobApi(url=config.url)
+
 
 client = ApiClient()
--- crowleer/api/schemas.py
+++ crowleer/api/schemas.py
@@ -1,25 +1,18 @@
 from datetime import date
-
 from typing import Optional
 
 from pydantic import BaseModel, Field
 
 
+MAX_DESCRIPTION_LENGTH = 4096
+
+
 class Job(BaseModel):
-    uid: int = Field(ge=1)
+    uid: int
     name: str = Field(min_length=2)
     salary: Optional[int]
     region: Optional[str] = Field(min_length=2)
-    description: Optional[str] = Field(max_length=100)
-    date_published: date
+    description: Optional[str] = Field(max_length=MAX_DESCRIPTION_LENGTH)
+    date_published: Optional[date]
     date_expiring: Optional[date]
     url: str
-
-
-class Company(BaseModel):
-
-    uid: int = Field(ge=1)
-    name: str = Field(min_length=2)
-    region: str = Field(min_length=2)
-    category: str = Field(min_length=2)
-    description: str
